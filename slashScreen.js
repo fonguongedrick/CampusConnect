8fc24cd612e9431dcf4a49f51e9863cd6bf3ac08
Fonguong Edrick <nchombonuiedrick26@gmail.com>
2023-12-13 14:45:27 +0100

    #2

---
+++ slashScreen.js
@@ -0,0 +1,6 @@
+setTimeout(() => {
+    document.querySelector('.loading-container').style.display = 'none';
+    setTimeout(function(){
+        C:\Users\USER\flutter\campusConnect\screen.html.href= "landingpage.html";
+    },2000);
+}, 2000);
